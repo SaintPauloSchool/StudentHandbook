a0e1856497a03f465a92f9d02b848f2cdff9fbef
77dahuzicat77 <dahuzicat77@163.com>
2026-02-03 16:35:23 +0800

    修改dev的sql配置

--- sp-api/src/main/java/com/sp/web/controller/WeChatWorkOAuthController.java
+++ sp-api/src/main/java/com/sp/web/controller/WeChatWorkOAuthController.java
@@ -10,7 +10,6 @@ import com.sp.system.service.DepartmentParentBindingService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
