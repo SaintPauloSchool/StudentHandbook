58ace6634c18bd01a3414ca30a89a286d604ee3a
77dahuzicat77 <dahuzicat77@163.com>
2026-02-03 10:54:33 +0800

    prod場啟用驗證

--- student-handbook-vue/src/config/settings.js
+++ student-handbook-vue/src/config/settings.js
@@ -1,10 +1,10 @@
 // 前端全局配置设置
 export default {
   // 微信验证开关
-  enableWeChatAuth: false,
+  enableWeChatAuth: true,
   
   // Token验证开关
-  enableTokenAuth: false,
+  enableTokenAuth: true,
   
   // 登录页面路径
   loginPath: '/login'
