8f66b6a30f87f95e14056859f802bbe74300ba14
xck1129 <dahuzicat77@163.com>
2026-03-26 16:19:16 +0800

    修改dev的配置

--- student-handbook-vue/src/config/settings.js
+++ student-handbook-vue/src/config/settings.js
@@ -1,11 +1,11 @@
 // 前端全局配置设置
 export default {
-  // 微信验证开关
-  enableWeChatAuth: true,
-  
-  // Token验证开关
-  enableTokenAuth: true,
-  
-  // 登录页面路径
-  loginPath: '/login'
+    // 微信验证开关
+    enableWeChatAuth: false,
+
+    // Token验证开关
+    enableTokenAuth: false,
+
+    // 登录页面路径
+    loginPath: '/login'
 }
