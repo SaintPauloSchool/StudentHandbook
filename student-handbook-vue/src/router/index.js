903a9be79ef19ff3c4b7f816242a95f7a77ed0a2
xck1129 <dahuzicat77@163.com>
2026-04-16 16:04:28 +0800

    先注釋家校通知功能

--- student-handbook-vue/src/router/index.js
+++ student-handbook-vue/src/router/index.js
@@ -1,6 +1,6 @@
 import {createRouter, createWebHistory} from 'vue-router'
 import Home from '../views/Home.vue'
-import StudentHandbook from '../components/StudentHandbook.vue'
+import StudentHandbook from '../views/StudentHandbook.vue'
 import Login from '../views/Login.vue'
 // import ParentNotice from '../views/ParentNotice.vue'
 // import NoticeDetail from '../views/NoticeDetail.vue'
